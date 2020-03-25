b3a70d1827bdb13af6bde595a9da552b9d972ab4
dell <dell@DESKTOP-F5N4VKT>
2020-03-25 15:02:51 +0800

    第一次提交

---
+++ Test/TestProvider.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+class TestProvider {
+
+	@Test
+	void test() {
+		try {
+			//1、获得mybatis核心配置文件的输入流对象
+			InputStream is=Resources.getResourceAsStream("mybatis.xml");
+			//2、通过输入流来创建SqlSessionFactory对象
+			SqlSessionFactory fac=new SqlSessionFactoryBuilder().build(is);
+			//3、通过SqlSessionFactory对象来创建Sqlsession对象
+			SqlSession sqlsession=fac.openSession();
+			//4、调用SqlSession对象的方法来执行操作
+			int count=sqlsession.selectOne("dao.ProviderMapper.getCount");
+			System.out.println("获得的数据总数"+count);
+			//5、关闭sqlsession对象
+			sqlsession.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
